16fbec675db90df6491c3642724a878cca9d75e6
cemlevent54 <cemlevent54@gmail.com>
2024-07-28 12:01:29 +0300

    Add project files.

---
+++ DrinkDiscovery_Admin/Models/Context.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections.Generic;
+using Microsoft.EntityFrameworkCore;
+using System.Data.Sql;
+using Microsoft.EntityFrameworkCore.SqlServer;
+
+namespace DrinkDiscovery_Admin.Models
+{
+    public class Context : DbContext
+    {
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer("Data Source=DESKTOP-0PCHDQV;Initial Catalog=Db_DrinkDiscovery;Integrated Security=True;Encrypt=False");
+        }
+
+        public DbSet<Iceceklers> Icecekler { get; set; }
+        public DbSet<Urunlers> Urunler { get; set; }
+        public DbSet<Kullanicilars> Kullanicilar { get; set; }
+        public DbSet<Yorumlars> Yorumlar { get; set; }
+
+        public DbSet<Adminlers> Adminler { get; set; }
+    }
+}
---
+++ DrinkDiscovery_Admin/Models/Iceceklers.cs
@@ -0,0 +1,19 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace DrinkDiscovery_Admin.Models
+{
+    public class Iceceklers
+    {
+        [Key]
+        public int icecek_id { get; set; }
+        public string icecek_ad { get; set; }
+        public byte[] icecek_resim { get; set; }
+        public int icecek_fiyat { get; set; }
+        public string icecek_icerik { get; set; }
+        public string icecek_malzemeler { get; set; }
+        public string icecek_puan { get; set; }
+        public IList<Yorumlars> icecek_yorumlar { get; set; }
+
+
+    }
+}
